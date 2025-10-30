e17a6242222620e6dbab1e247b5fa598390c1ebf
makatt <loshamaks@gmail.com>
2025-10-30 18:33:10 +0300

    create project

---
+++ src/main/java/com/example/chessserverspringboot/ChessServerSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.chessserverspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessServerSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChessServerSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/chessserverspringboot/ChessServerSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chessserverspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessServerSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
