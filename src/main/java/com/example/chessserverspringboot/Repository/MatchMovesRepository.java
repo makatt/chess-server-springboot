ce5b0a90fb8908422108fd206fe19b0c5e867974
makatt <loshamaks@gmail.com>
2025-12-03 23:49:33 +0300

    a post was made for matches, match_moves

--- src/main/java/com/example/chessserverspringboot/Repository/MatchMovesRepository.java
+++ src/main/java/com/example/chessserverspringboot/Repository/MatchMovesRepository.java
@@ -3,6 +3,14 @@ package com.example.chessserverspringboot.Repository;
 
 import com.example.chessserverspringboot.Entity.MatchMove;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface MatchMovesRepository extends JpaRepository<MatchMove, Integer> {}
+import java.util.List;
+
+public interface MatchMovesRepository extends JpaRepository<MatchMove, Integer> {
+
+    @Query("SELECT mv FROM MatchMove mv WHERE mv.match_id = :matchId ORDER BY mv.move_number ASC")
+    List<MatchMove> findByMatchId(@Param("matchId") int matchId);
+}
 
